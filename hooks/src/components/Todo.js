fd8d21c285956bf7d96c8801a5daa4dd2f66a591
Perry <perrydev17@gmail.com>
2019-06-18 09:53:27 +0800

    initial setup todo list with hoooks useState

---
+++ hooks/src/components/Todo.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const todo = props => {
+    const [ todoName, setTodoName ] = useState('');  //is an array , first element is currentState, second element is a function
+    const [ todoList, setTodoList ] = useState([]);
+
+    //const [todoState, setTodoState] = useState({ userInput: '', todoList:[]});
+
+    const inputChangeHandler = (event) => {
+        //setTodoState({userInput: event.target.value, todoList: todoState.todoList});
+
+        setTodoName(event.target.value);
+    };
+
+    const todoAddHandler = () => {
+        //setTodoState({userInput: todoState.userInput, todoList: todoState.todoList.concat(todoName));
+        setTodoList(todoList.concat(todoName));
+    }
+
+    return  <React.Fragment>
+        <input  type="text" 
+                placeholder="" 
+                onChange={inputChangeHandler}
+                value={todoName}
+        />
+        <button type="button" onClick={todoAddHandler}>Add</button>
+        <ul>
+            {todoList.map(todo => (
+                <li key={todo}>
+                    {todo}
+                </li>
+            ))}
+        </ul>
+    </React.Fragment>
+};
+
+export default todo;
